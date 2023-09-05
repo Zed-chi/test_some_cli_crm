f2536daed5b37f78177ff053c8008651f7652957
zed-chi <zed-chi@mail.ru>
2023-09-05 18:36:05 +0300

    app

---
+++ app.py
@@ -0,0 +1,44 @@
+from menu import MenuItem
+from config import db
+from models import Contract, Project
+from contracts.options import contracts
+from projects.options import projects
+
+from colorama import just_fix_windows_console
+just_fix_windows_console()
+def init():
+    db.connect()
+    db.create_tables([Contract, Project])
+
+
+"""
+Logic:
+1.Project_menu
+    - Create
+    - Work with
+        - Add_contract
+        - Finish project contract
+    - List All
+2.Contract_menu
+    - Create
+    - Sign
+    - Finish 
+    - List All
+3. List Info
+4.Exit
+"""
+
+
+def main():
+    item = MenuItem("Main Menu", root=True)
+    item.add_child(projects)
+    item.add_child(contracts)
+    try:
+        item.loop()
+    except KeyboardInterrupt:
+        print("Bye")
+
+
+if __name__ == "__main__":
+    init()
+    main()
