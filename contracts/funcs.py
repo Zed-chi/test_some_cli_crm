918a74de1941aeb43fd80ca231dea62ef3471638
zed-chi <zed-chi@mail.ru>
2023-09-05 21:19:02 +0300

    changed func for testing

--- contracts/funcs.py
+++ contracts/funcs.py
@@ -2,6 +2,8 @@ from datetime import datetime
 
 from color_print import ColorPrint as cprint
 from models import Contract
+from exceptions import CancelException
+from peewee import IntegrityError
 
 
 def list_contracts(status=None):
@@ -13,45 +15,56 @@ def list_contracts(status=None):
         cprint.print_fail("No contracts")
         return
     cprint.print_pass("--- Data ---")
-    cprint.print_info(f"id\ttitle\tstatus")
+    cprint.print_info(f"id\tstatus\ttitle")
     for i in contracts:
-        cprint.print_info(f"{i.id}\t{i.title}\t{i.status}")
+        cprint.print_info(f"{i.id}\t{i.status}\t{i.title}")
 
 
-def create_contract():
-    while True:
-        name = input("Type contract name: ")
-        if not name:
-            continue
-        try:
-            contract = Contract.create(
-                title=name, created_at=datetime.now(), status="draft"
-            )
-            cprint.print_pass(f"Contract {contract.id} created")
-            break
-        except Exception as e:
-            cprint.print_fail(e)
+def create_contract(name=None):
+    if not name:
+        while True:
+            name = input("Type contract name: ")
+            if name:
+                break
+        
+    try:
+        contract = Contract.create(
+            title=name, created_at=datetime.now(), status="draft"
+        )
+        return contract
+        cprint.print_pass(f"Contract {contract.id} created")
+    except IntegrityError:
+        cprint.print_fail("Name in use")
+    except Exception as e:
+        cprint.print_fail(e)
 
 
-def sign_contract():
+def sign_contract(contract=None):
     list_contracts(status="draft")
+    if not contract:
+        try:
+            contract = get_contract_from_cli_id()
+        except CancelException:
+            return
+    contract.status = "active"
+    contract.signed_at = datetime.now()
+    contract.save()
+    cprint.print_pass("Contract signed")
+
+
+def get_contract_from_cli_id():
     while True:
         _id = input("Type contract id to sign (or 0 to go back): ")
         if _id == "0":
-            return
+            raise CancelException()
         if not _id:
             cprint.print_fail("Empty data")
             continue
         try:
             contract = Contract.get(id=_id)
-            contract.status = "active"
-            contract.signed_at = datetime.now()
-            contract.save()
-            cprint.print_pass("Contract signed")
-            break
+            return contract
         except Exception as e:
             cprint.print_fail(e)
-
 
 def finish_contract():
     while True:
