93a2c131058811d976acd1ce20676d8f195dde10
zed-chi <zed-chi@mail.ru>
2023-09-05 22:33:09 +0300

    commented finish feature

--- contracts/options.py
+++ contracts/options.py
@@ -15,6 +15,6 @@ contracts = MenuItem("Contracts")
 
 
 contracts.add_child(MenuItem(title="List All", func=funcs.list_contracts))
-contracts.add_child(MenuItem(title="Finish", func=funcs.finish_contract))
+# contracts.add_child(MenuItem(title="Finish", func=funcs.finish_contract))
 contracts.add_child(MenuItem(title="Sign", func=funcs.sign_contract))
 contracts.add_child(MenuItem(title="Create", func=funcs.create_contract))
